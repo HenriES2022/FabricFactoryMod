2e46f17c23ff2cf57e6d5275e78b2e1a9bd661b5
henri <henriqueyeguo.yeguo@gmail.com>
2022-07-10 20:25:56 +0200

    Project package name changes

--- src/main/java/factorymod/ExampleMod.java
+++ src/main/java/factorymod/ExampleMod.java
@@ -1,4 +1,4 @@
-package net.henries2022.factorymod;
+package factorymod;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
--- src/main/java/factorymod/mixin/ExampleMixin.java
+++ src/main/java/factorymod/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.henries2022.factorymod.mixin;
+package factorymod.mixin;
 
-import net.henries2022.factorymod.ExampleMod;
+import factorymod.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
